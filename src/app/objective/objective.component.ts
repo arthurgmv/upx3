de95149e4f46d9c25bbb6bc0e9a58ac153aa5f6d
Arthur Gabriel <arthurgabrielmv@gmail.com>
2024-05-15 21:16:23 -0300

    initial commit

--- src/app/objective/objective.component.ts
+++ src/app/objective/objective.component.ts
@@ -8,12 +8,22 @@ import { Component } from '@angular/core';
 })
 export class ObjectiveComponent {
 objTitle:string = "Energia Solar Inteligente";
-objText:string = `Bacon ipsum dolor amet chislic pork belly ham, rump biltong short ribs hamburger ribeye prosciutto bacon corned beef pancetta ham hock tri-tip.
-  Spare ribs pancetta pork loin ribeye chicken. Turkey swine pastrami capicola venison. Flank tail bacon, boudin pork loin alcatra ground round rump chislic kevin kielbasa. Chuck jerky pork loin, cow picanha beef pork belly pastrami jowl venison turkey meatball. Boudin cow hamburger sausage short loin porchetta t-bone frankfurter.
+objText:string = `No século XXI, a questão energética tornou-se uma das principais preocupações globais,
+impulsionada pela necessidade de mitigar as mudanças climáticas, garantir o acesso
+universal à energia e promover o desenvolvimento econômico sustentável.
+Nesse contexto, as energias renováveis emergem como uma solução promissora,
+oferecendo uma fonte de energia limpa, abundante e acessível.
 
-  Rump buffalo drumstick shoulder fatback tri-tip swine doner t-bone pancetta. Boudin rump venison tail bresaola pork loin buffalo fatback t-bone frankfurter burgdoggen
-  ham hock bacon. Tongue turkey fatback shoulder capicola pastrami. Pig jowl beef ribs salami, capicola shoulder ground round turkey fatback corned beef. Jerky landjaeger kevin rump drumstick ribeye sirloin. Sausage boudin bacon, andouille leberkas pork short ribs strip steak meatloaf buffalo jerky cow. Cow bresaola cupim pork belly jerky.
-  `;
+A energia solar é uma forma limpa de energia, pois não emite poluentes atmosféricos durante a geração de eletricidade.
+ O uso de placas fotovoltaicas, conforme destacado na figura 1, ajuda a reduzir as emissões de gases de efeito estufa,
+ contribuindo assim para mitigar as mudanças climáticas e seus impactos negativos.
+
+ Ao gerar eletricidade a partir da luz solar, os sistemas fotovoltaicos oferecem uma fonte de energia descentralizada
+ e independente da rede elétrica. Isso pode ser especialmente útil em áreas remotas ou rurais, onde a conexão à rede é difícil ou cara.
+
+ Para alcançar o objetivo de obter energia solar através de uma placa fotovoltaica de maneira portátil e prática, foi utilizado
+ o Arduino UNO`;
+
 objUpd:string = "Last updated 3 mins ago"
 }
 
